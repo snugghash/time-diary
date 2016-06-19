e659780701705a27c308e2ee050bedc6deff2e7b
snugghash <snugghash@gmail.com>
2016-06-20 02:04:13 +0530

    Added store,display time when clicking on element.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+if (Modernizr.localstorage) {
+  // window.localStorage is available!
+} else {
+  // no native support for HTML5 storage :(
+  // maybe try dojox.storage or a third-party solution
+}
+var s = Snap("#timeSVG");
+var bigCircle = s.circle(150, 150, 100);
+bigCircle.click(function(f) {
+    bigCircle.attr({fill:"#ccc"});
+    window.localStorage["startTime"] = new Date().toLocaleString();
+    s.text(100,100, window.localStorage["startTime"]);
+});
