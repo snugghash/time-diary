11a78dbc277042b8f58f5a8d4a32585d702c85cb
snugghash <snugghash@gmail.com>
2016-06-26 23:59:59 +0530

    SQSH Cleanup

--- js/main.js
+++ js/main.js
@@ -6,17 +6,18 @@ if (Modernizr.localstorage) {
 }
 let s = Snap("#timeSVG");
 let bigCircle = s.circle(150, 150, 100);
-var isTracking = false;
-var isTracking = (function() {
-    if(window.localStorage["isTracking"] === null) {
-        alert(JSON.stringify(false));
+let isTracking = (function() {
+    if(window.localStorage.getItem("isTracking") === null) {
         window.localStorage["isTracking"] = JSON.stringify(false);
-        alert("wololo");
         return false; 
     }
-    else return window.localStorage["isTracking"];
-})();
-alert(isTracking);
+    else return JSON.parse(window.localStorage["isTracking"]);
+})(); 
+var textStartDisplay, moveDownGroup;
+body.onload = function() {
+    
+}
+
 bigCircle.click(function(f) {
     if(isTracking==true) {
         isTracking=false;
@@ -28,24 +29,18 @@ bigCircle.click(function(f) {
     window.localStorage["startTime"] = new Date().getTime();
     window.localStorage["startTimeString"] = new Date().toLocaleString();
     bigCircle.animate({r:25},100);
-    let textStartDisplay = s.text(250,150, window.localStorage["startTimeString"]);
-    var moveDownGroup = s.group(textStartDisplay);
+    textStartDisplay = s.text(250,150, window.localStorage["startTimeString"]);
+    moveDownGroup = s.group(textStartDisplay);
     function shiftDown() {
-        // increase sive of SVG element to accomodate new objects
+        // Increase size of SVG element to accomodate new objects
         s.node.style.height = parseInt(s.node.style.height)+50;
         newPosition = parseInt(textStartDisplay.attr('y')) + 50;
         // Draw new objects
-        var smallCircle = s.circle(150,newPosition,25);
+        let smallCircle = s.circle(150,newPosition,25);
         // Move down old objects
-        moveDownGroup = s.group(textStartDisplay,s.circle(150,newPosition,25));
+        moveDownGroup = moveDownGroup.add(smallCircle);
         textStartDisplay.animate({y:newPosition},100);
     }
     setInterval(shiftDown,1000);
-
 });
-window.onpageshow = function() {
-    alert("page show");
-}
-window.onpagehide = function() {
-    alert("page hidden");
-}
+
