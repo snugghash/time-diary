0985d0485821c6b3b568388fd2131885fb74fc58
snugghash <snugghash@gmail.com>
2017-07-10 09:39:03 +0530

    Added in Vega-based bar chart of tags vs.time.
    Has tooltips for correct time.
    TODO remove/add categories (on click?)
    TODO support for comparing only categories in same hierarchy level.

---
+++ js/reports.js
@@ -0,0 +1,101 @@
+if (Modernizr.localstorage) {
+  // window.localStorage is available!
+} else {
+  // no native support for HTML5 storage :(
+  // maybe try dojox.storage or a third-party solution
+}
+
+
+let timedTags = null;
+if(window.localStorage.getItem("tagTimes") === null) {
+    ;
+}
+else {
+  timedTags = JSON.parse(window.localStorage["tagTimes"]);
+}
+
+let chart = {
+  "$schema": "https://vega.github.io/schema/vega/v3.0.json",
+  "width": 400,
+  "height": 200,
+  "padding": 5,
+  "autosize": "pad",
+  "data": [
+    {
+      "name": "Tag times",
+      "values": timedTags,
+    }
+  ],
+  "scales": [
+    {
+      "name": "xscale",
+      "type": "band",
+      "domain": {"data": "Tag times", "field": "tag"},
+      "range": "width",
+      "padding": 0.05,
+      "round": true
+    },
+    {
+      "name": "yscale",
+      "domain": {"data": "Tag times", "field": "time"},
+      "nice": true,
+      "range": "height"
+    }
+  ],
+  "axes": [
+    { "orient": "bottom", "scale": "xscale" },
+    { "orient": "left", "scale": "yscale" }
+  ],
+  "marks": [
+    {
+      "type": "rect",
+      "from": {"data":"Tag times"},
+      "encode": {
+        "enter": {
+          "x": {"scale": "xscale", "field": "tag"},
+          "width": {"scale": "xscale", "band": 1},
+          "y": {"scale": "yscale", "field": "time"},
+          "y2": {"scale": "yscale", "value": 0}
+        },
+        "update": {
+          "fill": {"value": "steelblue"}
+        },
+        "hover": {
+          "fill": {"value": "red"}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "encode": {
+        "enter": {
+          "align": {"value": "center"},
+          "baseline": {"value": "bottom"},
+          "fill": {"value": "#333"}
+        },
+        "update": {
+          "x": {"scale": "xscale", "signal": "tooltip.tag", "band": 0.5},
+          "y": {"scale": "yscale", "signal": "tooltip.time", "offset": -2},
+          "text": {"signal": "tooltip.time"},
+          "fillOpacity": [
+            {"test": "datum === tooltip", "value": 0},
+            {"value": 1}
+          ]
+        }
+      }
+    },
+  ],
+  "signals": [
+    {
+      "name": "tooltip",
+      "value": {},
+      "on": [
+        {"events": "rect:mouseover", "update": "datum"},
+        {"events": "rect:mouseout",  "update": "{}"}
+      ]
+    }
+  ],
+}
+
+vega.embed('#chart', chart);
+
