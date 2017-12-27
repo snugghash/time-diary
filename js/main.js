b3ddd09ba01fb1f81282d24d96b06f86c78a6453
snugghash <snugghash@gmail.com>
2017-12-26 22:54:23 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,173 +1,12 @@
-if (Modernizr.localstorage) {
-  // window.localStorage is available!
-} else {
-  // no native support for HTML5 storage :(
-  // maybe try dojox.storage or a third-party solution
-}
-//let s = Snap("#timeSVG");
-//let timeSVG = $('#timeSVG');
-
-const secondHeight = 10;
-const minuteHeight = 20;
-const hourHeight = 50;
-const dayHeight = 100;
-
-//let bigCircle = s.circle(150, 150, dayHeight); // TODO fix in beta, starting and ending UX to be better
 let isTracking = (function() {
   if(window.localStorage.getItem("isTracking") === null) {
     window.localStorage["isTracking"] = JSON.stringify(false);
-    return false; 
+    return false;
   }
   else return JSON.parse(window.localStorage["isTracking"]);
-})(); 
-var textStartDisplay, moveDownGroup, updateTimeStepCaller;
+})();
 
-/**
- * Store given time slice data into localStorage
- */
-let storeEntry = function (startTime, endTime, description, tags) {
-  // TODO remove unnecessary row number
-  let rowNumber = null;
-  if(window.localStorage.getItem("rowNumber") === null) {
-    window.localStorage["rowNumber"] = 1;
-    rowNumber = 1;
-  }
-  else {
-    rowNumber = window.localStorage["rowNumber"] + 1;
-  }
-  let entries = [];
-  if(window.localStorage.getItem("entries") === null) {
-    ;
-  }
-  else {
-    entries = JSON.parse(window.localStorage["entries"]);
-  }
-  entries.push({startTime, endTime, description, tags});
-  window.localStorage["entries"] = JSON.stringify(entries);
-  window.localStorage["rowNumber"] = rowNumber;
 
-  // Collect all tags into a localStorage array.
-  let tagsList = ["Games", "Organizing"];
-  if(window.localStorage.getItem("tagList") === null) {
-    ;
-  }
-  else {
-    tagsList.push(JSON.parse(window.localStorage["tagsList"]));
-  }
-  tagsList = new Set(tagsList);
-  tags.forEach( (item) => {
-    tagsList.add(item);
-  });
-  tagsList = Array.from(tagsList);
-  console.log("All tags:", tagsList);
-  window.localStorage["tagsList"] = JSON.stringify(tagsList);
-
-  // Add to times for each tag
-  let tagTimes = []
-  if(window.localStorage.getItem("tagTimes") === null) {
-    ;
-  }
-  else {
-    tagTimes = JSON.parse(window.localStorage["tagTimes"]);
-  }
-  tags.forEach( (item) => {
-    if(item in tagTimes)
-      tagTimes[tagTimes.indexOf(item)].time += endTime - startTime;
-    else
-      tagTimes.push({
-        tag: item,
-        time: endTime - startTime
-      });
-  });
-  window.localStorage["tagTimes"] = JSON.stringify(tagTimes);
-};
-
-/**
- * Parse the tags out from the text description, rn just stuff ending with ';' or starting with '#'
- */
-let getTags = function (description) {
-  return description.split(" ").filter(function (word) {
-    return word.slice(-1) == ";";
-  }).map(function (word) {
-    return word.slice(0, -1);
-  });
-};
-
-let bigCircleOnClick = function(f) {
-  if(isTracking==true) {
-    isTracking=false;
-    window.localStorage["isTracking"] = JSON.stringify(false);
-    clearInterval(updateTimeStepCaller);
-    window.localStorage["endTime"] = new Date().getTime();
-    window.localStorage["endTimeString"] = new Date().toLocaleString();
-    let textEndDisplay = s.text(250,150,window.localStorage["endTimeString"]);
-    return;
-  }
-  isTracking=true;
-  window.localStorage["isTracking"] = JSON.stringify(true);
-  bigCircle.attr({fill:"#ccc"});
-  window.localStorage["startTime"] = new Date().getTime();
-  window.localStorage["startTimeString"] = new Date().toLocaleString();
-  bigCircle.animate({r:dayHeight/2},100);
-  textStartDisplay = s.text(250,150, window.localStorage["startTimeString"]);
-  moveDownGroup = s.group(textStartDisplay);
-  updateTimeStepCaller = setInterval(updateTimeStep,1000);
-};
-
-/*
- * Functions to call every time step
- */
-function updateTimeStep() {
-  enlargeSVG(secondHeight);
-  appendElement(secondHeight);
-  appendElement(secondHeight);
-}
-
-/*
- * Enlarge the SVG by the amount in the argument
- */
-function enlargeSVG(deltaHeight) {
-  let timeSVG = $('#timeSVG');
-  // Increase size of SVG element to accommodate new objects
-  timeSVG.height(timeSVG.height() + deltaHeight);
-  //If objects are out of sync with current time, draw them all at once.
-  // let i = (parseInt(s.node.style.height)-350)/50 - (parseInt(textStartDisplay.attr('y'))-150)/50;
-  /*while (i>0) {
-    console.log("Not synced");
-    appendElement();
-    i--;
-  }*/
-}
-
-/*
- * Append a time measure to the display
- */
-function appendElement(elementHeight) {
-  newPosition = parseInt(textStartDisplay.attr('y')) + elementHeight;
-  console.log("Append at " + newPosition);
-  // Draw new objects
-  let smallRect = s.rect(150-elementHeight/2, newPosition, elementHeight, elementHeight-3);
-  smallRect.attr({
-    fill: "#5050ff",
-    opacity:"0.4",
-  });
-  // Listen for time slice events.
-  smallRect.node.ondblclick = function(event) {
-    endTime = new Date().getTime() - 1000*(event.target.attributes['y'].value - 150)/elementHeight; 
-    startTime = localStorage["startTime"];
-    console.log("Sliced at " + endTime + " from " + startTime);
-    // Ask user for description of the time slice.
-    let description = prompt("Journal entry for the time slice until " + new Date(endTime).toLocaleString());
-    storeEntry(startTime, endTime, description, getTags(description));
-    window.localStorage["startTime"] = new Date().getTime();
-    window.localStorage["startTimeString"] = new Date().toLocaleString();
-    exportData();
-  };
-  let smallCircle = s.circle(150, newPosition, elementHeight/2);
-  // Move down old objects
-  moveDownGroup = moveDownGroup.add(smallCircle, smallRect);
-  textStartDisplay.animate({y:newPosition},100);
-}
 
 /* Gets us a well-formatted CSV file from a JSON array, with each object separated by newline, and each key omitted (values are used in fields of a row).
  * Credits: https://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable#8924856
@@ -295,6 +134,8 @@ $('#importData').on('change', function() {
       else {
         entries = JSON.parse(window.localStorage["entries"]);
       }
+      let maximum_time = 0; //JSON.parse(window.localStorage["startTime"]); // Prolly causes error if null TODO
+      console.log("init max time", maximum_time);
       for(let j=0; j < jsonArr.length - 1; j++) {
         let exists = false;
         temp = {
@@ -303,6 +144,9 @@ $('#importData').on('change', function() {
           description: jsonArr[j][2],
           tags: jsonArr[j].slice(3)
         };
+        if(temp.endTime > maximum_time) {
+          maximum_time = temp.endTime;
+        }
         for (let k=0; k < entries.length; k++) {
           if(entries[k].startTime == temp.startTime &&
              entries[k].endTime == temp.endTime) {
@@ -315,13 +159,16 @@ $('#importData').on('change', function() {
             }
             else {
               entries[k].description = entries[k].description.slice(0,-1) +" "+ temp.description + '"';
-              console.log("Same time slice, different description, finally:", entries[k].description);
+              console.log("Same time slice, different description; entry ", k, ", finally:", entries[k].description);
             }
           }
         }
         if(exists == false)
           entries.push(temp);
       }
+      console.log("final max time", maximum_time);
+      window.localStorage["startTime"] = JSON.stringify(maximum_time);
+      window.localStorage["startTimeString"] = maximum_time.toLocaleString();
       entries = [...new Set(entries)];
       console.log(entries);
       importConfirm = prompt("Check log output, import? true/false");
--- js/reports.js
+++ js/reports.js
@@ -1,3 +1,4 @@
+
 if (Modernizr.localstorage) {
   // window.localStorage is available!
 } else {
@@ -6,24 +7,21 @@ if (Modernizr.localstorage) {
 }
 
 
-let timedTags = null;
-if(window.localStorage.getItem("tagTimes") === null) {
-    ;
-}
-else {
-  timedTags = JSON.parse(window.localStorage["tagTimes"]);
-}
+
+// TODO remove storage for timedTags in localStorage
+let currentTime = new Date().getTime();
+timedTagsAllTime = getTimesForTags(0, currentTime);
 
 let chart = {
   "$schema": "https://vega.github.io/schema/vega/v3.0.json",
-  "width": timedTags.length*10,
+  "width": timedTagsAllTime.length*10,
   "height": 200,
   "padding": 5,
   "autosize": "pad",
   "data": [
     {
       "name": "Tag times",
-      "values": timedTags,
+      "values": timedTagsAllTime,
     }
   ],
   "scales": [
@@ -97,5 +95,204 @@ let chart = {
   ],
 }
 
-vega.embed('#chart', chart);
+vega.embed('#chartAllTime', chart);
 
+
+// TODO Numbers for tags don't work. tagTimes.indexOf(2) gives -1 even if 2 was in tagTimes.
+// Current workaround is to chnage entries to not have those.
+function getTimesForTags(startTime, endTime) {
+  let tagsArrayTmp = [];
+  let tagTimes = [];
+  let entries = JSON.parse(window.localStorage["entries"]);
+  entries.forEach( (entry) => {
+    if(entry.endTime >  startTime && entry.startTime < endTime) {
+      entry.tags.forEach( (tag) => {
+        if(tagsArrayTmp.includes(tag)) {
+          tagTimes[tagsArrayTmp.indexOf(tag)].time += entry.endTime - entry.startTime;
+        }
+        else {
+          tagsArrayTmp.push(tag);
+          tagTimes.push({
+            tag: tag,
+            time: entry.endTime - entry.startTime
+          });
+        }
+      });
+    }
+  });
+  return tagTimes;
+}
+
+
+
+timedTagsLast24Hours = getTimesForTags(currentTime - 1000*60*60*24, currentTime);
+
+let chartLast24Hours = {
+  "$schema": "https://vega.github.io/schema/vega/v3.0.json",
+  "width": timedTagsLast24Hours.length*10,
+  "height": 200,
+  "padding": 5,
+  "autosize": "pad",
+  "data": [
+    {
+      "name": "Tag times",
+      "values": timedTagsLast24Hours,
+    }
+  ],
+  "scales": [
+    {
+      "name": "xscale",
+      "type": "band",
+      "domain": {"data": "Tag times", "field": "tag"},
+      "range": {"step": 60},
+      "padding": 0.05,
+      "round": true
+    },
+    {
+      "name": "yscale",
+      "domain": {"data": "Tag times", "field": "time"},
+      "nice": true,
+      "range": "height"
+    }
+  ],
+  "axes": [
+    { "orient": "bottom", "scale": "xscale", "title": "Tags" },
+    { "orient": "left", "scale": "yscale", "title": "Time (ms)" }
+  ],
+  "marks": [
+    {
+      "type": "rect",
+      "from": {"data":"Tag times"},
+      "encode": {
+        "enter": {
+          "x": {"scale": "xscale", "field": "tag"},
+          "width": {"scale": "xscale", "band": 1},
+          "y": {"scale": "yscale", "field": "time"},
+          "y2": {"scale": "yscale", "value": 0}
+        },
+        "update": {
+          "fill": {"value": "steelblue"}
+        },
+        "hover": {
+          "fill": {"value": "red"}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "encode": {
+        "enter": {
+          "align": {"value": "center"},
+          "baseline": {"value": "bottom"},
+          "fill": {"value": "#333"}
+        },
+        "update": {
+          "x": {"scale": "xscale", "signal": "tooltip.tag", "band": 0.5},
+          "y": {"scale": "yscale", "signal": "tooltip.time", "offset": -2},
+          "text": {"signal": "tooltip.time"},
+          "fillOpacity": [
+            {"test": "datum === tooltip", "value": 0},
+            {"value": 1}
+          ]
+        }
+      }
+    },
+  ],
+  "signals": [
+    {
+      "name": "tooltip",
+      "value": {},
+      "on": [
+        {"events": "rect:mouseover", "update": "datum"},
+        {"events": "rect:mouseout",  "update": "{}"}
+      ]
+    }
+  ],
+}
+
+vega.embed('#chartLast24Hours', chartLast24Hours);
+
+timedTagsLast7Days = getTimesForTags(currentTime - 1000*60*60*24*7, currentTime);
+let chartLast7Days = {
+  "$schema": "https://vega.github.io/schema/vega/v3.0.json",
+  "width": timedTagsLast7Days.length*10,
+  "height": 200,
+  "padding": 5,
+  "autosize": "pad",
+  "data": [
+    {
+      "name": "Tag times",
+      "values": timedTagsLast7Days,
+    }
+  ],
+  "scales": [
+    {
+      "name": "xscale",
+      "type": "band",
+      "domain": {"data": "Tag times", "field": "tag"},
+      "range": {"step": 60},
+      "padding": 0.05,
+      "round": true
+    },
+    {
+      "name": "yscale",
+      "domain": {"data": "Tag times", "field": "time"},
+      "nice": true,
+      "range": "height"
+    }
+  ],
+  "axes": [
+    { "orient": "bottom", "scale": "xscale", "title": "Tags" },
+    { "orient": "left", "scale": "yscale", "title": "Time (ms)" }
+  ],
+  "marks": [
+    {
+      "type": "rect",
+      "from": {"data":"Tag times"},
+      "encode": {
+        "enter": {
+          "x": {"scale": "xscale", "field": "tag"},
+          "width": {"scale": "xscale", "band": 1},
+          "y": {"scale": "yscale", "field": "time"},
+          "y2": {"scale": "yscale", "value": 0}
+        },
+        "update": {
+          "fill": {"value": "steelblue"}
+        },
+        "hover": {
+          "fill": {"value": "red"}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "encode": {
+        "enter": {
+          "align": {"value": "center"},
+          "baseline": {"value": "bottom"},
+          "fill": {"value": "#333"}
+        },
+        "update": {
+          "x": {"scale": "xscale", "signal": "tooltip.tag", "band": 0.5},
+          "y": {"scale": "yscale", "signal": "tooltip.time", "offset": -2},
+          "text": {"signal": "tooltip.time"},
+          "fillOpacity": [
+            {"test": "datum === tooltip", "value": 0},
+            {"value": 1}
+          ]
+        }
+      }
+    },
+  ],
+  "signals": [
+    {
+      "name": "tooltip",
+      "value": {},
+      "on": [
+        {"events": "rect:mouseover", "update": "datum"},
+        {"events": "rect:mouseout",  "update": "{}"}
+      ]
+    }
+  ],
+}
+vega.embed('#chartLast7Days', chartLast7Days);
