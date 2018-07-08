86452623fb09b7042295dba8a6137813d6878b45
snugghash <snugghash@gmail.com>
2018-07-08 16:32:45 -0400

    Add reporting graphs for arbitrary datetime range

--- public/js/reports.js
+++ public/js/reports.js
@@ -328,3 +328,109 @@ let chartLast7Days = {
   ],
 }
 vega.embed('#chartLast7Days', chartLast7Days);
+
+// https://stackoverflow.com/a/14950153/
+(function(window, document, undefined){
+  window.onload = init;
+  function init(){
+    let tmp = document.getElementById("updateChartButton");
+    tmp.addEventListener('click', getTimesAndUpdateChart);
+  }
+})(window, document, undefined);
+
+function getArbitStartAndEndTimes() {
+  let arbitTimes = {};
+  arbitTimes.start = new Date(document.getElementById("arbitStartTime").value);
+  arbitTimes.end = new Date(document.getElementById("arbitEndTime").value);
+  console.log("start", arbitTimes.start.toLocaleString(), arbitTimes.end.toLocaleString());
+
+  return arbitTimes;
+}
+
+function getTimesAndUpdateChart() {
+  const arbitTimes = getArbitStartAndEndTimes();
+  timedTagsArbit = getTimesForTags(arbitTimes.start, arbitTimes.end);
+  let chartArbitTime = {
+    "$schema": "https://vega.github.io/schema/vega/v3.0.json",
+    "width": timedTagsArbit.length*10,
+    "height": 200,
+    "padding": 5,
+    "autosize": "pad",
+    "data": [
+      {
+        "name": "Tag times",
+        "values": timedTagsArbit,
+      }
+    ],
+    "scales": [
+      {
+        "name": "xscale",
+        "type": "band",
+        "domain": {"data": "Tag times", "field": "tag"},
+        "range": {"step": 60},
+        "padding": 0.05,
+        "round": true
+      },
+      {
+        "name": "yscale",
+        "domain": {"data": "Tag times", "field": "time"},
+        "nice": true,
+        "range": "height"
+      }
+    ],
+    "axes": [
+      { "orient": "bottom", "scale": "xscale", "title": "Tags" },
+      { "orient": "left", "scale": "yscale", "title": "Time (ms)" }
+    ],
+    "marks": [
+      {
+        "type": "rect",
+        "from": {"data":"Tag times"},
+        "encode": {
+          "enter": {
+            "x": {"scale": "xscale", "field": "tag"},
+            "width": {"scale": "xscale", "band": 1},
+            "y": {"scale": "yscale", "field": "time"},
+            "y2": {"scale": "yscale", "value": 0}
+          },
+          "update": {
+            "fill": {"value": "steelblue"}
+          },
+          "hover": {
+            "fill": {"value": "red"}
+          }
+        }
+      },
+      {
+        "type": "text",
+        "encode": {
+          "enter": {
+            "align": {"value": "center"},
+            "baseline": {"value": "bottom"},
+            "fill": {"value": "#333"}
+          },
+          "update": {
+            "x": {"scale": "xscale", "signal": "tooltip.tag", "band": 0.5},
+            "y": {"scale": "yscale", "signal": "tooltip.time", "offset": -2},
+            "text": {"signal": "tooltip.time"},
+            "fillOpacity": [
+              {"test": "datum === tooltip", "value": 0},
+              {"value": 1}
+            ]
+          }
+        }
+      },
+    ],
+    "signals": [
+      {
+        "name": "tooltip",
+        "value": {},
+        "on": [
+          {"events": "rect:mouseover", "update": "datum"},
+          {"events": "rect:mouseout",  "update": "{}"}
+        ]
+      }
+    ],
+  }
+  vega.embed('#chartArbitTime', chartArbitTime);
+}
