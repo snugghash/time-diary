7fb494d5de18bfcab89f7065f4b01aa27e3a7b38
snugghash <snugghash@gmail.com>
2018-07-10 07:20:48 -0400

    Had to resort to using hardlinks to use the same utils file

--- public/js/reports-old.js
+++ public/js/reports-old.js
@@ -1,3 +1,5 @@
+import getTags from './utilities.js'
+
 // TODO UX for choosing arbitrary time, maybe like chrone dev tools > perfprmance > time selector. Using average text UX for now.
 // Or some weird log-scaled version for allowing days and months on the same element.
 // Maybe only months are necessary to be selected this way - last 7 days and last day adequate for short time scales.
@@ -177,41 +179,3 @@ function getTimesForTags(startTime, endTime) {
     return b.time - a.time
   });
 }
-
-
-
-/**
- * Parse the tags out from the text description, rn just words ending with ';'
- * or starting with '#'. TODO NLP, reuse snugghash/ephemeron perhaps
- * TODO combine the both this and fn from App.js into utilities
- * TODO code between the two has diverged
- */
-function getTags(description) {
-  removeEdgeDoubleQuotes = function(description) {
-    let newDesc = description;
-    if(description.slice(0,1) == "\"") {
-      newDesc = newDesc.slice(1);
-    }
-    if(description.slice(-1) == "\"") {
-      newDesc = newDesc.slice(0,-1);
-    }
-    return newDesc;
-  }
-  description = removeEdgeDoubleQuotes(description);
-  let endTags = description.split(" ").filter(function (word) {
-    return word.slice(-1) === ";";
-  }).map(function (word) {
-    // TODO should we remove this?
-    if(word[0] == '"') {
-      return word.slice(1,-1);
-    }
-    return word.slice(0, -1);
-  });
-  let startTags = description.split(" ").filter(function (word) {
-    return word.charAt(0) === "#";
-  }).map(function (word) {
-    return word.slice(1);
-  });
-  // console.log(new Set([...endTags], [...startTags])); // TODO convert to test
-  return [...new Set([...endTags], [...startTags])]
-};
